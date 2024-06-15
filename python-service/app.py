8ae293d444b9fb033b497b569a68a035840bb967
Alfonso Hernandez <a.hernandez@newxel.online>
2024-06-15 22:58:46 +0200

    feat: make UploadController

--- python-service/app.py
+++ python-service/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from controllers.upload_controller import upload_bp
+from controllers.upload_controller import UploadController
 
 app = Flask(__name__)
-app.register_blueprint(upload_bp)
+app.register_blueprint(UploadController()())
 
 if __name__ == '__main__':
     app.run(debug=True)
--- python-service/controllers/upload_controller.py
+++ python-service/controllers/upload_controller.py
@@ -4,22 +4,27 @@ from app.table_processor import TableProcessor
 from app.excel_file import ExcelFile
 from app.file_upload_service import FileUploadService
 
-upload_bp = Blueprint('upload', __name__)
-file_upload_service = FileUploadService()
+class UploadController:
+    def __init__(self):
+        self._upload_bp = Blueprint('upload', __name__)
+        self.file_upload_service = FileUploadService()
+        self._upload_bp.add_url_rule('/upload', 'upload_file', self.upload_file, methods=['POST'])
 
-@upload_bp.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"}), 400
-    file = request.files['file']
-    try:
-        filepath = file_upload_service.save_file(file)
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
-    # Process the uploaded file
-    excel_file = ExcelFile(Path(filepath))
-    sheet = excel_file.get_sheet('Analysis Output')
-    table_processor = TableProcessor(sheet)
-    all_tables_data = table_processor.process_tables()
-    
-    return jsonify(all_tables_data)
+    def upload_file(self):
+        if 'file' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        file = request.files['file']
+        try:
+            filepath = self.file_upload_service.save_file(file)
+        except ValueError as e:
+            return jsonify({"error": str(e)}), 400
+        # Process the uploaded file
+        excel_file = ExcelFile(Path(filepath))
+        sheet = excel_file.get_sheet('Analysis Output')
+        table_processor = TableProcessor(sheet)
+        all_tables_data = table_processor.process_tables()
+        
+        return jsonify(all_tables_data)
+
+    def __call__(self):
+        return self._upload_bp
