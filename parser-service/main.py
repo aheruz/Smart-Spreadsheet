9e8baa147571835cb6ec46ab528b2f2fd40b3b6f
Alfonso Hernandez <a.hernandez@newxel.online>
2024-06-15 19:02:02 +0200

    chore: moving service to python-parser

--- parser-service/main.py
+++ parser-service/main.py
@@ -1,11 +1,14 @@
 import json
+import os
 from flask import jsonify
 from pathlib import Path
 from src.table_processor import TableProcessor
 from src.excel_file import ExcelFile
 
+os.environ['PYTHONPATH'] = os.path.dirname(os.path.abspath(__file__))
 # Process the uploaded file
-excel_file = ExcelFile(Path('tests/example_2.xlsx'))
+path = os.environ['PYTHONPATH']
+excel_file = ExcelFile(Path(f'{path}/tests/example_2.xlsx'))
 sheet = excel_file.get_sheet('Analysis Output')
 table_processor = TableProcessor(sheet)
 all_tables_data = table_processor.process_tables()
